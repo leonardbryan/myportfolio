d84fc791b0c4b346611dd32ba1130cce1504a762
Leonard <leonard23bryan@gmail.com>
2021-09-14 02:38:46 +0800

    Font Family and Body BG color

--- main.js
+++ main.js
@@ -4,36 +4,34 @@ const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
 
+
 // toggle button for Dark Mode theme
-const secLinks = document.querySelector(".navbar-nav");
-const dmBgNavBar = document.querySelector("#dmNavBar");
-const dmName = document.querySelector("#dmName");
-const bgIcon = document.querySelector("#bgIcon");
-const changeColor = document.querySelector("#moonIcon");
-bgIcon.addEventListener("click", function(){
-  changeColor.classList.toggle("text-secondary");
-  bgIcon.classList.toggle("bg-white");
-  dmName.classList.toggle("text-light");
-  dmBgNavBar.classList.toggle("bg-secondary");
-  secLinks.classList.toggle("text-whites");
-})
+// const secLinks = document.querySelector(".navbar-nav");
+// const dmBgNavBar = document.querySelector("#dmNavBar");
+// const dmName = document.querySelector("#dmName");
+// const bgIcon = document.querySelector("#bgIcon");
+// const changeColor = document.querySelector("#moonIcon");
+// bgIcon.addEventListener("click", function(){
+//   changeColor.classList.toggle("text-secondary");
+//   bgIcon.classList.toggle("bg-white");
+//   dmName.classList.toggle("text-light");
+//   dmBgNavBar.classList.toggle("bg-secondary");
+//   secLinks.classList.toggle("text-whites");
+// })
 
+// class Tao {
+//     constructor (firstName,lastName,bt){
+//       this.firstName = firstName;
+//       this.lastName = lastName;
+//       this.bt = bt;
+//     }
 
-
-class Tao {
-
-    constructor (firstName,lastName,bt){
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.bt = bt;
-    }
-
-    getFullname(){
+//     getFullname(){
       
-      return `${this.firstname} ${this.lastName} ${this.bt}`;
-    }
-}
+//       return `${this.firstname} ${this.lastName} ${this.bt}`;
+//     }
+// }
 
-const tao1 = new Tao("Leonard","Bryan",1);
-const tao2 = new Tao("Mark","Winston",2);
-console.log(tao1.firstName);
+// const tao1 = new Tao("Leonard","Bryan",1);
+// const tao2 = new Tao("Mark","Winston",2);
+// console.log(tao1.firstName);
