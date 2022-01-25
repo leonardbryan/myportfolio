491a8fb3ce4ea93a7f77b8c91896fb9dd419e548
Leonard <leonard23bryan@gmail.com>
2022-01-25 16:12:05 +0800

    added clear field function in js

--- main.js
+++ main.js
@@ -6,10 +6,13 @@ checkbox.addEventListener('change',() => {
 
 var toastTrigger = document.getElementById('liveToastBtn')
 var toastLiveExample = document.getElementById('liveToast')
+let inputs = document.querySelectorAll('input');
+let tArea = document.querySelectorAll('textarea');
 if (toastTrigger) {
   toastTrigger.addEventListener('click', function () {
     var toast = new bootstrap.Toast(toastLiveExample)
-
+    inputs.forEach(input => input.value = '');
+    tArea.forEach(textarea => textarea.value = '');
     toast.show()
   })
 }
