fdd01087d0c0ca5763860e47196705d8de7a8c82
Leonard <leonard23bryan@gmail.com>
2022-01-25 12:43:51 +0800

    added pdf cv viewer

--- main.js
+++ main.js
@@ -3,3 +3,20 @@ const checkbox = document.getElementById('checkbox');
 checkbox.addEventListener('change',() => {
     document.body.classList.toggle('dark');
 } );
+
+var toastTrigger = document.getElementById('liveToastBtn')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+
+    toast.show()
+  })
+}
+
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
